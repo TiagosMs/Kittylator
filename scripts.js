95118daab00721686ffeced6b82621901e04b80c
TiagosMs <tiagogames051@gmail.com>
2024-03-28 16:14:30 -0300

    100% pronto, eu acho

--- scripts.js
+++ scripts.js
@@ -30,19 +30,3 @@ function calcular()
         alert('nada pra calcular')
     }
 }
-
-  // Função para exibir um prompt e manipular o input
-  function exibirPrompt() {
-    // Utiliza o prompt para obter a entrada do usuário
-    var userInput = prompt("Seu nome meu docinho: ");
-
-    // Verifica se o usuário clicou em "Cancelar" ou não digitou nada
-    if (userInput === null || userInput === "") {
-        alert("Você não digitou nada.");
-    } else {
-        // Exibe o que foi digitado na página
-        document.getElementById("nome").innerHTML = "Olá " + userInput + ". Provavelmente não sabe matemática básica.";    }
-}
-
-// Chama a função quando a página carrega (apenas para fins de exemplo)
-window.onload = exibirPrompt;
